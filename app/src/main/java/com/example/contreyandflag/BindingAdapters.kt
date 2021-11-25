b10402d0edc9d61c1a3a4478474ecb9897289d2c
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-11-25 11:02:24 +0300

    homework-add country Flag

--- app/src/main/java/com/example/contreyandflag/BindingAdapters.kt
+++ app/src/main/java/com/example/contreyandflag/BindingAdapters.kt
@@ -4,15 +4,37 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.ImageLoader
+import coil.decode.SvgDecoder
 import coil.load
+import coil.request.ImageRequest
+import com.example.contreyandflag.network.FlagPhoto
 
+//@BindingAdapter("imageUrl")
+//fun bindImage(imgView: ImageView, imgUrl: String?) {
+//    imgUrl?.let {
+//        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+//        imgView.load(imgUri) {
+//            placeholder(R.drawable.loading_animation)
+//            error(R.drawable.ic_baseline_broken_image_24)
+//        }
+//    }
+//}
 @BindingAdapter("imageUrl")
-fun bindImage(imgView: ImageView, imgUrl: String?) {
-    imgUrl?.let {
-        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
-        imgView.load(imgUri) {
-            placeholder(R.drawable.loading_animation)
-            error(R.drawable.ic_baseline_broken_image_24)
-        }
+fun bindImage(imgView: ImageView, imgUrl: String?){
+    imgUrl.let {
+        val imageLoader = ImageLoader.Builder(imgView.context)
+            .componentRegistry { add(SvgDecoder(imgView.context)) }
+            .build()
+
+        val request = ImageRequest.Builder(imgView.context)
+            .data(imgUrl)
+            .target(imgView)
+            .build()
+
+        imageLoader.enqueue(request)
     }
+
+
 }
